9aa24b48ce98b26435f75ad34fd6c1b704ea689d
Denis Titusov <sescusu@gmail.com>
2018-11-30 15:10:02 +0500

    add dockerfile

--- main.go
+++ main.go
@@ -4,27 +4,31 @@ import (
 	"fmt"
 	"math/rand"
 	"net/http"
-	"strings"
 	"time"
 )
 
-const waitMilliseconds = 10000
+const (
+	waitMilliseconds = 10000
+	port             = ":8080"
+)
 
 var randomWait int32
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
-	message := r.URL.Path
-	message = strings.TrimPrefix(message, "/")
-	message = fmt.Sprintf("Wait till start: %d", randomWait)
+	message := fmt.Sprintf("Wait till start: %d", randomWait)
 
 	w.Write([]byte(message))
 }
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	randomWait = waitMilliseconds - rand.Int31n(waitMilliseconds/2)
-	time.Sleep(time.Duration(randomWait) * time.Microsecond)
+	timeToSleep := time.Duration(randomWait) * time.Millisecond
+	fmt.Printf("[%v] Time to sleep: %v\n", time.Now().Format("2006-01-02 15:04:05.000"), timeToSleep)
+	time.Sleep(timeToSleep)
+	fmt.Printf("[%v] Start listener at port %s\n", time.Now().Format("2006-02-01 15:04:05.000"), port)
 	http.HandleFunc("/", sayHello)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	if err := http.ListenAndServe(port, nil); err != nil {
 		panic(err)
 	}
 }
